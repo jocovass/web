cfbba68f809c76f8e4598cc690f730297d2958b7
joco.codes <joco.udv@gmail.com>
2020-08-07 22:05:22 +0100

    Setting the Theme and initial GlobalStyles (#1)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import Layout from '../components/Layout/Layout';
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+ function Home() {
+  return (
+    <Layout>
+      Hello world!
+    </Layout>
+  )
+};
+
+export default Home;
